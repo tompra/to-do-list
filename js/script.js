e7b7ae073c1fb301496e23a563af17c905a0cb10
tompra <tomprayon@gmail.com>
2023-09-12 14:56:00 +0200

    Add jQuery and steps to follow

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('check, check, check...')
+// Features of the to-do-list
+
+// 1. The user can add a new item to a list of items
+
+// 2. The user can cross out an item form the list of items
+
+// 3. The user can delete an item from the list of items
+
+// 4. The user can change the order of items in the list of items
+
+/////////////////////////////////////////////////////////////////////////
+
+// Logic steps
+// 1. Add event listener to the add button
+// 2. When clicking this should add a item to the ordered list
+// 3. Every item added must have an X, to delete the item
+// 4. When clicking twice, the item must have a crossing style, like it was done
+// 5. The user can drag the item and change the position of it.
+
+/////////////////////////////////////////////////////////////////////////
+
+//1.
